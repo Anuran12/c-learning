9994002149912bef3618f2eb91751202a3615607
anuran12 <anuranarts@gmail.com>
2022-05-02 21:04:36 +0530

    Rules of Variables

---
+++ Chapter-1/Variables.c
@@ -0,0 +1,27 @@
+# include<stdio.h>
+
+int main() {
+
+    int number = 25;
+    char star = '*';
+    int age = 18;
+    float pi = 3.14;
+
+// Variables are case sensitive | 'a' store different value and 'A' store different value//
+    int a = 30;
+    int A =40;
+
+//1st character is alphabet or '_' |  {int 1 = 1; this is wrong} //
+    int age =18;
+    int _age = 19;
+    int _1 = 20;
+
+// no comma/blank space and no other symbol other then '_' |
+// {int first price = 100; int first,price = 100; these are wrong }
+    int final_price = 100;
+
+//variables can be changed// 
+    age = 20;
+    
+    return 0;
+}
