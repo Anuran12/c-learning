42153733da018fcc54c6be4b9bfd088cacc194c1
anuran12 <anuranarts@gmail.com>
2022-05-03 12:08:56 +0530

    Output

---
+++ Chapter-1/3.Output.c
@@ -0,0 +1,26 @@
+# include<stdio.h>
+
+int main() {
+
+//Output
+    printf("Anuran");
+    
+
+//multiple output
+    printf("Anuran");
+    printf("Anuran");
+    printf("Anuran");
+    printf("Anuran");
+
+
+//multiple output in different line
+    printf("Anuran\n");
+    printf("Anuran\n");
+    printf("Anuran\n");
+    printf("Anuran\n");
+
+
+
+    return 0;
+
+}
