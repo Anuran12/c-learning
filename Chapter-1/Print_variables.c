c370ef53c147f9e3d986d77fc37eb070dbc1ae71
anuran12 <anuranarts@gmail.com>
2022-05-03 12:01:19 +0530

    Print different variables

---
+++ Chapter-1/Print_variables.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+
+int main() {
+
+//Print integer variable- %d
+    int age = 18;
+    printf("age is %d", age);
+
+//Print real number variable- %f
+    float pi = 3.14;
+    printf("value is %f", pi);
+
+//Print character variable- %c
+    char star = '*';
+    printf("star looks like %c", star);
+
+
+    return 0;
+}
