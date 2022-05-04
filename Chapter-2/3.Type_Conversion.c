8282ac1e43c3c2813cc64ae6519a9ff0604c17de
anuran12 <anuranarts@gmail.com>
2022-05-04 18:35:04 +0530

    Types of conventions

---
+++ Chapter-2/3.Type_Conversion.c
@@ -0,0 +1,22 @@
+# include<stdio.h>
+
+int main() {
+
+// int *operator* int => int
+    printf("%d\n", 2 * 2);//output should be integer-"%d"
+
+// int *operator* float => float
+    printf("%f\n", 2 * 2.1);//output shuld be float-"%f"
+
+// float *operator* float => float
+    printf("%f\n", 2.1 * 2.2);//output shuld be float-"%f"
+
+
+//EXEPTIONS
+    printf("%f\n", 3.0/2);//don't put 3/2
+
+    int a = (int) 1.999999;//explicit conversion
+    printf("%d\n", a);
+
+    return 0;
+}
